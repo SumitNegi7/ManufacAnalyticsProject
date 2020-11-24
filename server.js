a0ae15d2ca73ef592ffe31a5228391141099b33f
Sumit <sumit@pop-os.localdomain>
2020-11-24 10:58:58 +0530

    Server Error

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ const PORT = process.env.PORT || 5000;
 
 if(process.env.NODE_ENV ==="production"){
     // server static content
-    // npm run build
+    // yarn run build
     app.use(express.static(path.join(__dirname,"my-app/build"))) 
 }
 
